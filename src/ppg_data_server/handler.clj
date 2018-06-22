e340c156ad86dbdf1ed13714a78818322bb187a6
Caleb Phillips <calebrphillips@gmail.com>
2018-06-22 17:01:23 -0400

    Proper return status and dump incoming requests for viewing

--- src/ppg_data_server/handler.clj
+++ src/ppg_data_server/handler.clj
@@ -3,14 +3,17 @@
             [compojure.route :as route]
             [ring.middleware.defaults :refer [wrap-defaults api-defaults]]
             [ring.middleware.json :refer [wrap-json-body]]
+            [ring.util.response :refer [response status]]
             [ppg-data-server.data :as data]))
 
 (defroutes app-routes
   (GET "/" [] "Hello World")
-  (POST "/echo" req
+  (POST "/trials" req
+        (do (println req)
         (if (data/save (:body req))
-          "Success"
-          "Failure"))
+          (response "Success")
+          (status (response "Failure") 500)))
+        )
   (route/not-found "Not Found"))
 
 
