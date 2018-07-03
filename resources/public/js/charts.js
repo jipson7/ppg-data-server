913cbfec7d5d68d9c553af1b3512eca53b829242
Caleb Phillips <calebrphillips@gmail.com>
2018-07-03 19:56:13 -0400

    Update JS to use new format of data return

--- resources/public/js/charts.js
+++ resources/public/js/charts.js
@@ -27,15 +27,17 @@ function getTrial(trial) {
     var trialId = trial._id;
     $.get("/trials/" + trialId, function(data) {
         var obj = JSON.parse(data);
-        console.log(obj);
-        chartDevices(obj);
+        chartDevices(obj.devices);
     });
 }
 
-function datifyTimestamps(device) {
-    for (var key in device) {
-        if (device.hasOwnProperty(key)) {
-            device[key].map(v => {
+function datifyTimestamps(data){
+    for (var key in data) {
+        if (key == "_id") {
+            continue;
+        }
+        if (data.hasOwnProperty(key)) {
+            data[key].map(v => {
                 v.x = new Date(v.x);
                 return v;
             });
@@ -43,65 +45,64 @@ function datifyTimestamps(device) {
     }
 }
 
-function chartDevices(data) {
+function chartDevices(devices) {
     var charts = [];
 
-    if (data.hasOwnProperty(WRIST_DEVICE)) {
-        datifyTimestamps(data[WRIST_DEVICE]);
-        var lines = [{
-            type: "line",
-            dataPoints: data[WRIST_DEVICE].red,
-            legendText: "RED",
-            showInLegend: true,
-            color: "red"
-        }, {
-            type: "line",
-            dataPoints: data[WRIST_DEVICE].ir,
-            lineColor: "blue",
-            legendText: "IR",
-            showInLegend: true,
-            color: "blue"
-        }];
-        charts.push(createChart(lines, "Wrist LEDs", "wrist_device"));
-    }
-
-
-    if (data.hasOwnProperty(GROUND_TRUTH)) {
-        datifyTimestamps(data[GROUND_TRUTH]);
-        var lines = [{
-            type: "line",
-            dataPoints: data[GROUND_TRUTH].hr,
-            legendText: "HR",
-            showInLegend: true,
-            color: "red"
-        }, {
-            type: "line",
-            dataPoints: data[GROUND_TRUTH].oxygen,
-            legendText: "Oxygen",
-            showInLegend: true,
-            color: "blue"
-        }];
-        charts.push(createChart(lines, "GROUND TRUTH", "ground_truth"));
-    }
-
-    if (data.hasOwnProperty(FINGERTIP_SENSOR)) {
-        datifyTimestamps(data[FINGERTIP_SENSOR]);
-        var lines = [{
-            type: "line",
-            dataPoints: data[FINGERTIP_SENSOR].red,
-            legendText: "RED",
-            showInLegend: true,
-            color: "red"
-        }, {
-            type: "line",
-            dataPoints: data[FINGERTIP_SENSOR].ir,
-            legendText: "IR",
-            showInLegend: true,
-            color: "blue"
-        }];
-        charts.push(createChart(lines, "Fingertip LEDs", "fingertip_sensor"));
-    } else {
-        $("#fingertip_sensor").remove()
+    for (var i = 0; i < devices.length; i++) {
+        var device = devices[i];
+        var data = device.data;
+        datifyTimestamps(data);
+        if (device.type == WRIST_DEVICE) {
+            var lines = [{
+                type: "line",
+                dataPoints: data.red,
+                legendText: "RED",
+                showInLegend: true,
+                color: "red"
+            }, {
+                type: "line",
+                dataPoints: data.ir,
+                lineColor: "blue",
+                legendText: "IR",
+                showInLegend: true,
+                color: "blue"
+            }];
+            charts.push(createChart(lines, "Wrist LEDs", "wrist_device"));
+        } else if (device.type == GROUND_TRUTH) {
+            datifyTimestamps(data[GROUND_TRUTH]);
+            var lines = [{
+                type: "line",
+                dataPoints: data.hr,
+                legendText: "HR",
+                showInLegend: true,
+                color: "red"
+            }, {
+                type: "line",
+                dataPoints: data.oxygen,
+                legendText: "Oxygen",
+                showInLegend: true,
+                color: "blue"
+            }];
+            charts.push(createChart(lines, "GROUND TRUTH", "ground_truth"));
+        } else if (device.type == FINGERTIP_SENSOR) {
+            datifyTimestamps(data[FINGERTIP_SENSOR]);
+            var lines = [{
+                type: "line",
+                dataPoints: data.red,
+                legendText: "RED",
+                showInLegend: true,
+                color: "red"
+            }, {
+                type: "line",
+                dataPoints: data.ir,
+                legendText: "IR",
+                showInLegend: true,
+                color: "blue"
+            }];
+            charts.push(createChart(lines, "Fingertip LEDs", "fingertip_sensor"));
+        } else {
+            console.error("Unknown device type");
+        }
     }
 
     _CHARTS = charts;
